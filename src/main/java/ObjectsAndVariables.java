426eb122e9c5e188149e431f1f24b80a82b5ba68
kmoreti <kleber.camargo@fatec.sp.gov.br>
2016-12-26 21:46:14 -0200

    Initial commit

---
+++ src/main/java/ObjectsAndVariables.java
@@ -0,0 +1,13 @@
+/**
+ * Created by moreti on 12/26/16.
+ */
+public class ObjectsAndVariables {
+    public static void main(String[] args) {
+        String s1;
+        s1 = new String();
+
+        String s2 = new String();
+
+        s1 = "Yow!";
+    }
+}
