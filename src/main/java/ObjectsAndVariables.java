079d241b7d4f7e37e4565966aa4ff073426919cb
kmoreti <kleber.camargo@fatec.sp.gov.br>
2016-12-26 23:30:19 -0200

    End of the day #1

--- src/main/java/ObjectsAndVariables.java
+++ src/main/java/ObjectsAndVariables.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * Created by moreti on 12/26/16.
  */
@@ -9,5 +12,22 @@ public class ObjectsAndVariables {
         String s2 = new String();
 
         s1 = "Yow!";
+        s2 = s1; //s1 & s2 reference same object.
+
+        s2 = new String(s1);//s1 & s2 reference 2 different identical objects;
+
+        s2 = s1.toUpperCase();
+        System.out.println(s2);
+
+        String s3 = s2.concat("!!");
+        System.out.println(s3);
+
+        String s4 = "*".concat(s2).concat("*");
+        System.out.println(s4);
+
+        s4 = "&" + s2 + "&";
+        System.out.println(s4);
+
+        //Strings are immutable: their contents never change
     }
 }
---
+++ src/main/java/SimpleIO.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.URL;
+
+/**
+ * Created by moreti on 12/26/16.
+ */
+public class SimpleIO {
+    //System.out is a PrintStream object that outputs to the screen
+    //System.in is an InputStream object that reads from the keyboard
+    //readLine method is defined on BufferReader objects
+
+    //* How do we construct a BufferedReader? (Chars into entire lines of text)
+    //- Whith an InputStreamReader
+    //* How do we construct a InputStreamReader? (Compose into characters, typically 2 bytes)
+    //- We need an InputStream
+    //* How do we construct a InputStream? (Reads raw data)
+    //- System.in is one
+
+    public static void main(String[] args) throws Exception{
+//        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println(keyboard.readLine());
+
+        URL url = new URL("http://www.uol.com.br");
+        BufferedReader site = new BufferedReader(new InputStreamReader(url.openStream()));
+        String text = null;
+        while ((text = site.readLine()) != null) {
+            System.out.println(text);
+        }
+    }
+}
