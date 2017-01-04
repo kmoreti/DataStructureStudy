c7cbb7aae3e3b7cb6a68e89ac4487bb07b5cac83
kmoreti <kleber.camargo@fatec.sp.gov.br>
2017-01-04 00:08:25 -0200

    day #3

---
+++ src/main/java/day3/SList.java
@@ -0,0 +1,37 @@
+package day3;
+
+/**
+ * Created by moreti on 1/3/17.
+ */
+public class SList {
+    private  SListNode head;
+    private  int size;
+
+    public SList() {
+        head = null;
+        size = 0;
+    }
+
+    public void insertFront(Object item) {
+        head = new SListNode(item, head);
+        size ++;
+    }
+
+    public static void main(String[] args) {
+        SList list = new SList();
+        list.insertFront("fish");
+        list.insertFront("milk");
+        list.insertFront("fruit");
+        list.insertFront("soap");
+
+        SListNode next = list.head;
+        for (int i = 0; i < list.size; i++) {
+            if (next!= null) {
+                System.out.println(next.item);
+                next = next.next;
+            }
+        }
+    }
+
+    //55:27
+}
---
+++ src/main/java/day3/SListNode.java
@@ -0,0 +1,14 @@
+package day3;
+
+/**
+ * Created by moreti on 1/3/17.
+ */
+public class SListNode {
+    public Object item;
+    public SListNode next;
+
+    public SListNode (Object item, SListNode next) {
+        this.item = item;
+        this.next = next;
+    }
+}
