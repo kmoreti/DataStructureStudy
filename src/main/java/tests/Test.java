907879f0996da80a087368df996c36e18694fc66
kmoreti <kleber.camargo@fatec.sp.gov.br>
2017-01-23 22:50:54 -0200

    Lab3

--- src/main/java/tests/Test.java
+++ src/main/java/tests/Test.java
@@ -1,24 +1,31 @@
 package tests;
 
+import java.lang.management.ThreadInfo;
+
 /**
  * Created by moreti on 12/28/16.
  */
 public class Test {
     public static void main(String[] args) {
 
-        double x = 11.2 % 4.9;
+//        double x = 11.2 % 4.9;
+//
+//        System.out.println(x);
+//
+//        int y = Integer.parseInt("1678");
+//        System.out.println(y);
+//
+//        int i = 43;
+//        long l = 43L;
+//
+//        l = i;
+//        System.out.println(l);
+//        i = (int)l;
+//        System.out.println(i);
+        String s1, s2;
+        s1 = "Kleber";
+        s2 = "Kleber";
+        System.out.println(s1 == s2);
 
-        System.out.println(x);
-
-        int y = Integer.parseInt("1678");
-        System.out.println(y);
-
-        int i = 43;
-        long l = 43L;
-
-        l = i;
-        System.out.println(l);
-        i = (int)l;
-        System.out.println(i);
     }
 }
